9e9ca5c2e68ec792d49f65632c56d95e99317fe6
8rlley <korz.a.spb@gmail.com>
2022-02-09 13:42:08 +0300

    +sharedPrefs

--- lib/main.dart
+++ lib/main.dart
@@ -123,7 +123,44 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  
+  Widget taskBuilder() {
+    return Flexible(
+      child: SingleChildScrollView(
+        child: ListView.builder(
+            padding: EdgeInsets.zero,
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: taskList.length,
+            itemBuilder: (context, index) {
+              return Row(
+                children: [
+                  Checkbox(
+                      value: taskList[index].isCompleted,
+                      onChanged: (_) {
+                        setState(() {
+                          setCompletness(taskList[index]);
+                        });
+                      }),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    child: TextField(
+                      focusNode: taskList[index].taskFocusNode,
+                      controller: taskList[index].taskController,
+                      onSubmitted: (value) => setState(() {
+                        taskList[index].title = value;
+                        saveData();
+                      }),
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: taskList[index].title),
+                    ),
+                  ),
+                ],
+              );
+            }),
+      ),
+    );
+  }
 
   void setCompletness(Task task) {
     task.isCompleted = !task.isCompleted;
